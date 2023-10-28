35944e1fce10f9a4476d675c0265d0e1e2939ea0
runarut <runarut@users.noreply.github.com>
2023-10-28 10:05:16 -0500

    bug fix and apps do not increment boot counter.

--- bin/imu_logger.sh
+++ bin/imu_logger.sh
@@ -7,7 +7,7 @@ export APP_ID="imu"
 export APP_HOME="/home/$(whoami)/telemetry-data"
 export APP_TMP_DIR="${APP_HOME}/tmp"
 export APP_BASE_PATH="${APP_HOME}/data"
-export APP_PYTHON="/home/$(whoami).local/bin/python3.11"
+export APP_PYTHON="/home/$(whoami)/.local/bin/python3.11"
 export DEBUG="True"
 export SHARED_DICTIONARY_NAME="TELEMETRY"
 export RESTART_DELAY=30
@@ -16,7 +16,7 @@ export RESTART_DELAY=30
 export APP_COUNT=$(${APP_PYTHON} -m tcounter.app_counter ${APP_ID})
 
 # get current system startup counter
-export BOOT_COUNT=$(${APP_PYTHON} -m tcounter.boot_counter)
+export BOOT_COUNT=$(${APP_PYTHON} -m tcounter.boot_counter --current_boot_count)
 
 export APP_LOG_FILE="telemetry-${BOOT_COUNT}-${APP_ID}-${APP_COUNT}.log"
 
