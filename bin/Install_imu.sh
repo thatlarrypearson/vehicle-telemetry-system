4deb18c730b4ecd4e24a14a295f7dd1c46e34a8c
runarut <runarut@users.noreply.github.com>
2023-11-06 10:12:46 -0600

    add build/install script

---
+++ bin/Install_imu.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash
+# Install_imu.sh
+#
+
+export APP_HOME="/home/$(whoami)/telemetry-imu"
+export APP_PYTHON="/home/$(whoami)/.local/bin/python3.11"
+export DEBUG="True"
+
+# Debugging support
+if [ "${DEBUG}" = "True" ]
+then
+	# enable shell debug mode
+	set -x
+fi
+
+cd ${APP_HOME}
+
+if [ -d "${APP_HOME}/dist" ]
+then
+	rm -rf "${APP_HOME}/dist"
+fi
+
+${APP_PYTHON} -m build .
+ls -l dist/*.whl
+${APP_PYTHON} -m pip install --force-reinstall dist/*.whl
+
+${APP_PYTHON} -m imu_logger.imu_logger --help
+${APP_PYTHON} -m imu_logger.usb_devices --help
